ed881813137e8ab6db1820c9111856886816268d
unknown <eryk.patryk.kanownik@gmail.com>
2022-04-05 18:53:14 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const tile =  document.querySelector("#1");
+
+function playMusic(){
+    let src = "";
+    let MusicPlayer = new Audio();
+    
+}
+
+tile.addEventListener('hover',() => {
+    setTimeout(
+        ,3000)
+})
